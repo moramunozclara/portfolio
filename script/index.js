2aff7dc170732eeecd0ee4d0c666bb119d5020ff
Clara Mora <moramunozclara@alumnos.cei.es>
2024-07-18 18:20:16 +0200

    fix

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
     // Menú desplegable
     const headerBtn = document.querySelector('.Header-btn');
     const headerNav = document.querySelector('.Header-nav');
@@ -7,63 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         headerNav.classList.toggle('isActive');
         headerBtn.classList.toggle('isActive');
 
-        // headerNav.classList.toggle('isHidden');
     });
-
-  
-
-
-    // Galería de proyectos con filtros
-    const trabajosFilterAll = document.querySelector('.Trabajos-filter--all');
-    const trabajoPreviews = document.querySelectorAll('.Trabajo-preview');
-
-    const trabajosFilterUxui = document.querySelector('.Trabajos-filter--uxui');
-    const trabajosFilterGrafico = document.querySelector('.Trabajos-filter--grafico');
-    const trabajosFilter3d = document.querySelector('.Trabajos-filter--3d');
-
-    trabajosFilterAll.addEventListener('click', function () {
-        trabajoPreviews.forEach(function (preview) {
-            preview.classList.add('isActive');
-            preview.classList.remove('isHidden');
-        });
-    });
-
-    trabajosFilterUxui.addEventListener('click', function () {
-        trabajoPreviews.forEach(function (preview) {
-            if (preview.classList.contains('Trabajo-uxui')) {
-                preview.classList.add('isActive');
-                preview.classList.remove('isHidden');
-            } else {
-                preview.classList.remove('isActive');
-                preview.classList.add('isHidden');
-            }
-        });
-    });
-
-    trabajosFilterGrafico.addEventListener('click', function () {
-        trabajoPreviews.forEach(function (preview) {
-            if (preview.classList.contains('Trabajo-grafico')) {
-                preview.classList.add('isActive');
-                preview.classList.remove('isHidden');
-            } else {
-                preview.classList.remove('isActive');
-                preview.classList.add('isHidden');
-            }
-        });
-    });
-
-    trabajosFilter3d.addEventListener('click', function () {
-        trabajoPreviews.forEach(function (preview) {
-            if (preview.classList.contains('Trabajo-3d')) {
-                preview.classList.add('isActive');
-                preview.classList.remove('isHidden');
-            } else {
-                preview.classList.remove('isActive');
-                preview.classList.add('isHidden');
-            }
-        });
-    });
-
 
 
     // SLIDER IMAGENES
@@ -138,6 +83,60 @@ document.addEventListener('DOMContentLoaded', function () {
     sliderImages.addEventListener("mouseout", () => {
         agregarIntervalo();
     });
+
+    // Galería de proyectos con filtros
+    const trabajosFilterAll = document.querySelector('.Trabajos-filter--all');
+    const trabajoPreviews = document.querySelectorAll('.Trabajo-preview');
+
+    const trabajosFilterUxui = document.querySelector('.Trabajos-filter--uxui');
+    const trabajosFilterGrafico = document.querySelector('.Trabajos-filter--grafico');
+    const trabajosFilter3d = document.querySelector('.Trabajos-filter--3d');
+
+    trabajosFilterAll.addEventListener('click', function () {
+        trabajoPreviews.forEach(function (preview) {
+            preview.classList.add('isActive');
+            preview.classList.remove('isHidden');
+        });
+    });
+
+    trabajosFilterUxui.addEventListener('click', function () {
+        trabajoPreviews.forEach(function (preview) {
+            if (preview.classList.contains('Trabajo-uxui')) {
+                preview.classList.add('isActive');
+                preview.classList.remove('isHidden');
+            } else {
+                preview.classList.remove('isActive');
+                preview.classList.add('isHidden');
+            }
+        });
+    });
+
+    trabajosFilterGrafico.addEventListener('click', function () {
+        trabajoPreviews.forEach(function (preview) {
+            if (preview.classList.contains('Trabajo-grafico')) {
+                preview.classList.add('isActive');
+                preview.classList.remove('isHidden');
+            } else {
+                preview.classList.remove('isActive');
+                preview.classList.add('isHidden');
+            }
+        });
+    });
+
+    trabajosFilter3d.addEventListener('click', function () {
+        trabajoPreviews.forEach(function (preview) {
+            if (preview.classList.contains('Trabajo-3d')) {
+                preview.classList.add('isActive');
+                preview.classList.remove('isHidden');
+            } else {
+                preview.classList.remove('isActive');
+                preview.classList.add('isHidden');
+            }
+        });
+    });
+
+
+
 });
 
 
